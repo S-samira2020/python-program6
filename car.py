37d4cf3f6510cfb9c59f1bc0a7c1b9be9eab70d9
Samira <54632872+S-samira2020@users.noreply.github.com>
2020-09-19 10:50:00 +0600

    Add files via upload

---
+++ car.py
@@ -0,0 +1,14 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:20:19) [MSC v.1925 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> car = ' bmw '
+>>> car == ' bmw '
+True
+>>> car = ' Audi '
+>>> car == ' audi '
+False
+>>> car = ' Audi '
+>>> car.lower() == ' audi '
+True
+>>> car
+' Audi '
+>>> 
---
+++ program4.py
@@ -0,0 +1,5 @@
+r_topping = ' mushrooms '
+
+if r_topping  != ' cauliflower ':
+    print("Hold the cauliflower!")
+    
